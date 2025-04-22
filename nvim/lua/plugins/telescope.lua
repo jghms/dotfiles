8f3791099b6e6e71573363f2f7477ec295962d81
Jan-Gerrit Harms <jan.gerrit.harms@gmail.com>
2025-04-22 22:48:18 +0200

    use fd for finding files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,10 +5,12 @@ return {
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "make",
 		},
-		opt = {
+		opts = {
 			defaults = {
 				path_display = { shorten = 3 },
 				prompt_prefix = "> ",
+				find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--exclude", "node_modules" },
+				file_ignore_patterns = { "node_modules", ".git/", ".hg/", ".svn/", "__pycache__" },
 			},
 			extensions_list = { "fzf", "terms", "themes", "file_browser" },
 		}
